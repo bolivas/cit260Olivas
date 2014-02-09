0115d091ee108c6e532c096a08ce5d272a236e34
bolivas <bentolivas@yahoo.com>
2014-02-08 23:01:43 -0700

    Update MenuGame.java

--- MenuGame.java
+++ MenuGame.java
@@ -1,4 +1,5 @@
 
+
 package chkrs;
 
 import java.util.Scanner;
@@ -10,6 +11,7 @@ public class MenuGame {
     String instructions = "What dificulty of computer would you like to play?";
     String selection = "Default";
     int menuItems = 4;
+    boolean valid = false;
     public String[][] menus = {
         {"H", "Hard"}, 
         {"M", "Medium"},
@@ -17,16 +19,60 @@ public class MenuGame {
 	{"R", "Return to previous menu"},
 	};
    
+    private MenuGameControl gameMenuControl = new MenuGameControl();
+    private Chkrs returnTo = new Chkrs();
+    public MenuGame(){
+	
+    }
     public void displayMenu(){
 	System.out.println(this.instructions+"\n");
 	
 	for(int i = 0; i < this.menuItems; i++){
 	    System.out.println(this.menus[i][0]+ "   " +this.menus[i][1]);
 	}
-	
+    }
+    public void getInput(){
+	do{
+	displayMenu();
 	System.out.println("\nPlease make your selection now.");
-	this.selection = input.next().toUpperCase().trim();	
+	valid = getCommand(selection);
 	
+	if(valid){
+	    switch(selection){
+		case "H": this.gameMenuControl.hard();
+		    break;
+		case "M": this.gameMenuControl.medium();
+		    break;
+		case "E": this.gameMenuControl.easy();
+		    break;
+		case "R": this.returnTo.displayMenu();
+		    break;
+		}
+	}
+	}while (valid != true);
     }
     
+    public boolean getCommand(String string){
+	
+	
+	boolean valid = false;
+	do{
+	    selection = input.next().trim().toUpperCase();
+	    valid = validateCommand(selection);
+	    if(!valid){
+		new Error().displayError("Invalid command. Please enter a valid command");
+	    }
+	}while (!valid);
+	return true;
+    }
+    
+    private boolean validateCommand(String command){
+	
+	for(String[] row : this.menus){
+	    if(row[0].equals(selection)){
+		return true;
+	    }
+	}
+	return false;
+    }
 }
