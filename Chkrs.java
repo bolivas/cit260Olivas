80d05172ff875dddec4b21ff296240894197b0a1
bolivas <bentolivas@yahoo.com>
2014-03-06 18:26:00 -0700

    Update Chkrs.java

--- Chkrs.java
+++ Chkrs.java
@@ -1,81 +1,67 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package chkrs;
 
-import java.util.Scanner;
 
 /**
  *
  * @author bensmac
  */
 public class Chkrs {
-    Scanner input = new Scanner(System.in);
+   
     
-    String name = "bbbbbbb";
-    String instructions = "This a a java version of checkers. \nI Hope you enjoy playing this game."
-	    + "\nYou can use the help menu any time you want to get help. Have fun!!!";
-    int menuItems = 3;
-    public String[][] menus = {
-        {"S", "Start a new game."}, 
-        {"H", "Display the help menu."},
-	{"Q", "Quit and exit."},
-	};
-    String selection = "Default";
     
     public static void main(String[] args) {
-	int total = 0;
-	GetPoints getpoints = new GetPoints();
-	total = getpoints.getpoints();
-	if(total >= 0){
-	System.out.println(total);
-	}
-	Chkrs myGame = new Chkrs();
-	myGame.getName();
-	myGame.displayMenu();
-	myGame.action();
+	int r = 0;
+	int c = 0;
+	int ir = 0;
+	int ic = 0;
+	Intel intel = new Intel();
+	Intel2 intel2 = new Intel2();
+	//intel2.checkAPiece();
+	//intel2.compareValues();
+	Board board = new Board();
 	
+	/**/for(int i = 0; i < 2000; i++){
+	if(intel.forTheComWin == 12){
+	    System.out.println("Your simply not the best. The computers are the best.");
+	    break;
+	}
+	if(intel2.forTheHumWin == 12){
+	    System.out.println("Your simply the best. The humans are the best.");
+	    break;
+	}
+	board.displayBoard();
+	
+	
+	intel.run();
+	
+	System.out.println("\nComputer moves.\n");
+	board.displayBoard();	
+	
+	SelectRow selectRow = new SelectRow();
+	r = selectRow.getInput();
+	
+	SelectCol selectCol = new SelectCol();
+	c = selectCol.getStuff();
+	
+	SelectRowMove selectRowMove = new SelectRowMove();
+	ir = selectRowMove.getInput();
+	
+	SelectColMove selectColMove = new SelectColMove();
+	ic = selectColMove.getInput();
+	
+	
+	intel2.run(r, c, ir, ic);
+	}
+	/*
+	PlayerInfo playerinfo = new PlayerInfo();
+	playerinfo.getName();
+	
+	MenuMain menuMain = new MenuMain();
+	menuMain.getInput();
+	*/
 	
 	
     }
-    public void getName(){
-	
-	System.out.println("Please enter your name.");
-	this.name = input.next().trim();
-    }
-    public void displayMenu(){
-	System.out.println("\nWelcome "+ this.name + "\n");
-	System.out.println(this.instructions+"\n");
-	
-	for(int i = 0; i < this.menuItems; i++){
-	    System.out.println(this.menus[i][0]+ "   " +this.menus[i][1]);
-	}
-	
-	System.out.println("\nPlease make your selection now.");
-	this.selection = input.next().toUpperCase().trim();	
-	
-    }
-    public void action(){
-	System.out.println("----"+this.selection+"----");
-	if(this.selection == "S"){
-	    MenuGame start = new MenuGame();
-	    start.displayMenu();
-	    System.out.println("sssss");
-	}
-	else if(this.selection == "H"){
-	    MenuHelp help = new MenuHelp();
-	    System.out.println("Help is on the way.");
-	    
-	}
-	else if(this.selection == "Q"){
-	    System.out.println("Good Bye");
-	}
-	else{
-	    System.out.println("Incorrect Section");
-	}
-    }
-    
+   
 }
