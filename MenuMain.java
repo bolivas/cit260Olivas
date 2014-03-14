202139f51c8305a5ce6716d0aa539739a6be2f88
bolivas <bentolivas@yahoo.com>
2014-03-13 21:43:00 -0600

    Update MenuMain.java

--- MenuMain.java
+++ MenuMain.java
@@ -1,75 +1,32 @@
+
 package chkrs;
 
 import java.util.Scanner;
 
-/**
- *
- * @author bensmac
- */
-public class MenuMain {
-    Scanner input = new Scanner(System.in);
-    String instructions = "Welcome to Checkers! Plese select an option below to begin.";
-    String selection= "Default";
-    int menuItems = 2;
-    boolean valid = false;
+public class MenuMainHelp extends Menu{
+    
+    private static final String instructions = "What can we help you with?";
+    private static int menuItems = 2;
     private final static String[][] menus = {
-        {"N", "New Game"}, 
-	{"H", "Help Menu"},
+        {"1", "Game play instructions"}, 
+        {"2", "Menu Navigation"},
 	};
     
-    MenuMainHelp menuMainHelp = new MenuMainHelp();
-    MenuGame menuGame = new MenuGame();
-    public MenuMain(){
+    MenuMainHelp(){
+	super(instructions,menuItems,menus);
 	
-
     }
-   
-    public void getInput(){
-	do{
-	displayMenu();
-	System.out.println("\nPlease make your selection now.");
-	valid = getCommand(selection);
-
+    
+   @Override
+   public void executeCommands(boolean valid){
 	if(valid){
 	    switch(selection){
-		case "N": menuGame.getInput();
+		case "1": System.out.println("NNNNNN");//menuPlaying.getInput();
 		    break;
-		case "H": menuMainHelp.getInput();
+		case "2": System.out.println("HHHHHH");//menuMainHelp.getInput();
 		    break;
                 
 		}
 	}
-	}while (valid != true);
     }
-    
-    public void displayMenu(){
-	System.out.println(this.instructions+"\n");
-
-	for(int i = 0; i < this.menuItems; i++){
-	    System.out.println(MenuMain.menus[i][0]+ "   " +MenuMain.menus[i][1]);
-	}
-    }
-    
-    public boolean getCommand(String string){
-	
-	do{
-	    selection = input.next().trim().toUpperCase();
-	    valid = validateCommand(selection);
-	    if(!valid){
-		new Error().displayError("Invalid command. Please enter a valid command");
-	    }
-	}while (!valid);
-	return true;
-    }
-    
-    private boolean validateCommand(String command){
-
-	for(String[] row : MenuMain.menus){
-	    if(row[0].equals(selection)){
-		return true;
-	    }
-	}
-	return false;
-    }
-    
 }
