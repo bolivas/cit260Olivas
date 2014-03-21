1f57ce337963b87b27eab72cfb898dfa356321ee
bolivas <bentolivas@yahoo.com>
2014-03-20 22:40:03 -0600

    Update MenuMain.java

--- MenuMain.java
+++ MenuMain.java
@@ -1,4 +1,5 @@
-package chkrs;
+
+package cit260.checkers.menus;
 
 import java.util.Scanner;
 
@@ -11,14 +12,28 @@ public class MenuMain extends Menu{
         {"1", "New Game"}, 
 	{"2", "Help Menu"},
 	};
-    private static String instructions ="Welcome to Checkers! Plese select an option below to begin.";
+    private static final String instructions ="Welcome to Checkers! Plese select an option below to begin.";
     private static int menuItems = 2;
     
-    MenuMain(){
+    public MenuMain(){
 	super(instructions,menuItems,menus);
     }
+   
+        public String executeCommands(){
+	if(valid){
+	    switch(selection){
+		case "1": System.out.println("NNNNNN");//menuPlaying.getInput();
+		    break;
+		case "2": System.out.println("HHHHHH");//menuMainHelp.getInput();
+		    break;
+                
+		}
+	    
+	}return selection;
+    }
+
     @Override
-    public void executeCommands(boolean valid){
+    public void executeCommands(boolean valid) {
 	if(valid){
 	    switch(selection){
 		case "1": System.out.println("NNNNNN");//menuPlaying.getInput();
@@ -29,4 +44,16 @@ public class MenuMain extends Menu{
 		}
 	}
     }
+
+    @Override
+    public String DisplyInfo(boolean valid) {
+	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getInfo() {
+	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
 }
