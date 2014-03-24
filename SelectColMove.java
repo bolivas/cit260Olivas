984a7c5fface129742689dade2d9358fe3c8a86e
bolivas <bentolivas@yahoo.com>
2014-03-24 09:46:30 -0600

    Update SelectColMove.java

--- SelectColMove.java
+++ SelectColMove.java
@@ -4,8 +4,9 @@
  * and open the template in the editor.
  */
 
-package chkrs;
+package select;
 
+import chkrs.Error;
 import java.util.Scanner;
 
 /**
@@ -13,16 +14,60 @@ import java.util.Scanner;
  * @author bensmac
  */
 public class SelectColMove {
-    Scanner input = new Scanner(System.in);
-    String instructions = "Please Select the column of where you want to move the piece";
-    boolean valid = false;
+    private static Scanner input = new Scanner(System.in);
+    private static String instructions = "Please Select the column of where you want to move the piece";
+    private static boolean valid = false;
     private final static String[] validInputs = {"A","B","C","D","E","F","G","H","a","b","c","d","e","f","g","h"};
-    String selection;
-    public int converted;
+    private static String selection;
+    private static int converted;
+    
     public SelectColMove(){
 	
     }
-    public int getInput(){
+
+    public static Scanner getInput() {
+	return input;
+    }
+
+    public static void setInput(Scanner input) {
+	SelectColMove.input = input;
+    }
+
+    public static String getInstructions() {
+	return instructions;
+    }
+
+    public static void setInstructions(String instructions) {
+	SelectColMove.instructions = instructions;
+    }
+
+    public static boolean isValid() {
+	return valid;
+    }
+
+    public static void setValid(boolean valid) {
+	SelectColMove.valid = valid;
+    }
+
+    public static String getSelection() {
+	return selection;
+    }
+
+    public static void setSelection(String selection) {
+	SelectColMove.selection = selection;
+    }
+
+    public static int getConverted() {
+	return converted;
+    }
+
+    public static void setConverted(int converted) {
+	SelectColMove.converted = converted;
+    }
+    
+    
+    
+    public int getStuff(){
 	while(!valid){
 	System.out.println(instructions);
 	selection = input.next();
