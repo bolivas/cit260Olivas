1b314ccbfc48f61023b4afa292b2d17f7ab416dc
bolivas <bentolivas@yahoo.com>
2014-03-24 09:48:22 -0600

    Update Titles.java

--- Titles.java
+++ Titles.java
@@ -4,89 +4,25 @@
  * and open the template in the editor.
  */
 
-package select;
-
-import chkrs.Error;
-import java.util.Scanner;
+package chkrs;
 
 /**
  *
  * @author bensmac
  */
-public class SelectRowMove {
-    private static Scanner input = new Scanner(System.in);
-    private static String instructions = "Please Select the row where you want to move the piece.";
-    private static  boolean valid = false;
-    private final static String[] validInputs = {"0","1","2","3","4","5","6","7"};
-    private static String selection;
-    private static int converted;
+public enum Titles {
+    BOARD ("Checker Board"),
+    MAIN_MENU ("Main Menu"),
+    PLAYING_MENU ("Playing Menu");
     
-    public SelectRowMove(){
-	
-    }
-
-    public static Scanner getInput() {
-	return input;
-    }
-
-    public static void setInput(Scanner input) {
-	SelectRowMove.input = input;
-    }
-
-    public static String getInstructions() {
-	return instructions;
-    }
-
-    public static void setInstructions(String instructions) {
-	SelectRowMove.instructions = instructions;
-    }
-
-    public static boolean isValid() {
-	return valid;
-    }
-
-    public static void setValid(boolean valid) {
-	SelectRowMove.valid = valid;
-    }
-
-    public static String getSelection() {
-	return selection;
-    }
-
-    public static void setSelection(String selection) {
-	SelectRowMove.selection = selection;
-    }
-
-    public static int getConverted() {
-	return converted;
-    }
-
-    public static void setConverted(int converted) {
-	SelectRowMove.converted = converted;
+    String value;
+    
+    Titles(String value){
+	this.value = value;
     }
     
-    
-    
-    public int getStuff(){
-	while(!valid){
-	System.out.println(instructions);
-	selection = input.next();
-	valid = validateCommand(selection);
-	    if(valid){
-		converted = Integer.parseInt(selection);
-	    }
-	}
-	return converted;
+    public String getValue(){
+	return value;
     }
-    private boolean validateCommand(String selection){
-	for(int i = 0;i < 8;i++){
-	    if(validInputs[i].equals(selection)){
-		valid = true;
-	    }
-	}
-	if(!valid){
-	    new Error().displayError("Invalid command. Please enter a valid command");
-	}
-	return valid;
-    }
+    
 }
