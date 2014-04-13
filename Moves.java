e08ca7d3ca783d7bed8a4230ac62979c64ab89f4
bolivas <bentolivas@yahoo.com>
2014-04-12 21:08:00 -0600

    Create Moves.java

---
+++ Moves.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package intel;
+
+import cit260.checkers.frames.BoardGui;
+
+/**
+ *
+ * @author bensmac
+ */
+public class Moves {
+    public String[][] brd = {
+        {" ","O"," ","O"," ","O"," ","O"}, 
+        {"O"," ","O"," ","O"," ","O"," "},
+        {" ","O"," ","O"," ","O"," ","O"},
+	{" "," "," "," "," "," "," "," "},
+	{" "," "," "," "," "," "," "," "},
+	{"X"," ","X"," ","X"," ","X"," "},
+        {" ","X"," ","X"," ","X"," ","X"},
+	{"X"," ","X"," ","X"," ","X"," "},
+	};
+    public void movePiece(){
+	BoardGui gui = new BoardGui();
+	
+    }
+}
